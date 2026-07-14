4e58806c0297380e8ba89ac5533be3b7bdd2bf28
Narr the Reg <5944268+german77@users.noreply.github.com>
2026-07-14 17:02:24 -0600

    custom functions

--- lib/easy.c
+++ lib/easy.c
@@ -76,6 +76,8 @@
 #include "ssh.h"
 #include "curl_printf.h"
 
+#include "siglo/curl_nintendo_threads.h"
+
 /* The last #include files should be: */
 #include "curl_memory.h"
 #include "memdebug.h"
@@ -225,15 +227,7 @@ CURLcode curl_global_init(long flags)
   if(initialized++)
     return CURLE_OK;
 
-  /* Setup the default memory functions here (again) */
-  Curl_cmalloc = (curl_malloc_callback)malloc;
-  Curl_cfree = (curl_free_callback)free;
-  Curl_crealloc = (curl_realloc_callback)realloc;
-  Curl_cstrdup = (curl_strdup_callback)system_strdup;
-  Curl_ccalloc = (curl_calloc_callback)calloc;
-#if defined(WIN32) && defined(UNICODE)
-  Curl_cwcsdup = (curl_wcsdup_callback)_wcsdup;
-#endif
+  Curl_SigloAllocatorInitializeDefaults();
 
   if(flags & CURL_GLOBAL_SSL)
     if(!Curl_ssl_init()) {
@@ -284,6 +278,12 @@ CURLcode curl_global_init(long flags)
   return CURLE_OK;
 }
 
+CURLcode curl_global_init_internal()
+{
+  Curl_SigloMiddlewareInfo();
+  // TODO complete SSL and ThreadGC init
+}
+
 /*
  * curl_global_init_mem() globally initializes cURL and also registers the
  * user provided callback routines.
@@ -331,6 +331,7 @@ void curl_global_cleanup(void)
   if(--initialized)
     return;
 
+  Curl_SigloThreadGCFinalize();
   Curl_global_host_cache_dtor();
 
   if(init_flags & CURL_GLOBAL_SSL)
